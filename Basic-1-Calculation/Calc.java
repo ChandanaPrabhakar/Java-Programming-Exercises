06c973ef75e008390836ae8695d4e606a64b8326
Chandana <chandanaprabhakar.2000@gmail.com>
2024-05-14 14:09:32 +0100

    Added Basic calculation programs with i/o

---
+++ Basic-1-Calculation/Calc.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Calc{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter first number");
+        int a = in.nextInt();
+        System.out.println("Enter second number");
+        int b = in.nextInt();
+        System.out.println(a +" + "+b+" = "+ (a+b));
+        System.out.println(a +" - "+b+" = "+ (a-b));
+        System.out.println(a +" x "+b+" = "+ (a*b));
+        System.out.println(a +" / "+b+" = "+ (a/b));
+        System.out.println(a +" mod "+b+" = "+ (a%b));
+    }
+}
