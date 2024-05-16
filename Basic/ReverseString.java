94d84dfc522e49d4032706c24aa242f9bf2b12a2
Chandana <chandanaprabhakar.2000@gmail.com>
2024-05-16 16:44:36 +0100

    Updated ReverseString program

--- Basic/ReverseString.java
+++ Basic/ReverseString.java
@@ -6,9 +6,10 @@ public class ReverseString {
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the string: ");
-        String str = in.next();
+        String str = in.nextLine();
         char [] s = str.toCharArray();
-        for(int i=str.length()-1; i>0; i--){
+        System.out.println("The reversed string: ");
+        for(int i=str.length()-1; i>=0; i--){
             System.out.print(s[i]);
         }
     }
