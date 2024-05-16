61a0adf6628c0b7dc205e5ac539fd7564b69c6ca
Chandana <chandanaprabhakar.2000@gmail.com>
2024-05-16 16:44:17 +0100

    Added ASCII value program

---
+++ Basic/ASCIIvalue.java
@@ -0,0 +1,12 @@
+package Basic;
+import java.util.*;
+public class ASCIIvalue {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the character: ");
+        char ch = in.next().charAt(0);
+        int c = ch;
+        System.out.println("The ASCII value of "+ch+" is: "+c);
+    }
+    
+}
